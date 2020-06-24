d8213261eb2d212763b9145e4732942c289b753c
lihaifeng <2975745207@qq.com>
2020-06-24 11:48:03 +0800

    组件更新

---
+++ SpecComponent/NewViewController.h
@@ -0,0 +1,17 @@
+//
+//  NewViewController.h
+//  SpecComponent
+//
+//  Created by lihaifeng on 2020/6/24.
+//  Copyright © 2020 lihaifeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NewViewController : ViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SpecComponent/NewViewController.m
@@ -0,0 +1,35 @@
+//
+//  NewViewController.m
+//  SpecComponent
+//
+//  Created by lihaifeng on 2020/6/24.
+//  Copyright © 2020 lihaifeng. All rights reserved.
+//
+
+#import "NewViewController.h"
+
+@interface NewViewController ()
+
+@end
+
+@implementation NewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.title = @"pod测试";
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
